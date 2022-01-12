6d5ba2e8ef56a19aa26b3eea345035ce5ef45710
okonomiyakki <83577128+okonomiyakki@users.noreply.github.com>
2022-01-12 19:00:33 +0900

    Create poem.js

---
+++ poem.js
@@ -0,0 +1,23 @@
+function changeText0() {
+    var span = document.getElementsByTagName('span');
+    span[0].innerHTML="모두모두"
+}
+
+function changeText1() {
+    var span = document.getElementsByTagName('span');
+    span[1].innerHTML="각자"
+}
+
+function changeText2() {
+    var span = document.getElementsByTagName('span');
+    span[2].innerHTML="코로나 조심!"
+}
+
+function changeText3() {
+    var span = document.getElementsByTagName('span');
+    span[0].innerHTML = "모"
+    var span = document.getElementsByTagName('span');
+    span[1].innerHTML = "각"
+    var span = document.getElementsByTagName('span');
+    span[2].innerHTML="코"
+}
