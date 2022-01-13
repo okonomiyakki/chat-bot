52a1cc7c228fbb5ed575f14b6cd3cd3ae8d2766b
okonomiyakki <83577128+okonomiyakki@users.noreply.github.com>
2022-01-13 15:59:29 +0900

    Update

---
+++ js/chats.js
@@ -0,0 +1,4 @@
+function changeAns() {
+    var p = document.getElementsByTagName('p');
+    p[0].innerHTML="하이"
+}
--- js/poem.js
+++ js/poem.js
@@ -1,23 +1,21 @@
-function changeText0() {
+function changePoem0() {
     var span = document.getElementsByTagName('span');
     span[0].innerHTML="모두모두"
 }
 
-function changeText1() {
+function changePoem1() {
     var span = document.getElementsByTagName('span');
     span[1].innerHTML="각자"
 }
 
-function changeText2() {
+function changePoem2() {
     var span = document.getElementsByTagName('span');
     span[2].innerHTML="코로나 조심!"
 }
 
-function changeText3() {
+function changeAll() {
     var span = document.getElementsByTagName('span');
     span[0].innerHTML = "모"
-    var span = document.getElementsByTagName('span');
     span[1].innerHTML = "각"
-    var span = document.getElementsByTagName('span');
     span[2].innerHTML="코"
 }
